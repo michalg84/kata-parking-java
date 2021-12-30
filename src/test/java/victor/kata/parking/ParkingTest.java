e7021987f6475532fb6c18dfae706c8d06ed43db
Michał Gałka <michalgalka84@gmail.com>
2021-12-30 13:49:03 +0100

    fix and add more tests

--- src/test/java/victor/kata/parking/ParkingTest.java
+++ src/test/java/victor/kata/parking/ParkingTest.java
@@ -95,42 +95,46 @@ public class ParkingTest {
                         UUUUU""", parking.toString());
     }
 
-//    @Test
-//    public void testCompleteSolution() {
-//        assertEquals(7, parking.parkCar('C'));
-//        assertEquals("UUUUU\nU=CU@\n@U=UU\nUUUUU\nUUUUU", parking.toString());
-//        assertEquals(22, parking.getAvailableBaysCount());
-//
-//        assertEquals(9, parking.parkCar('C'));
-//        assertEquals("UUUUU\nC=CU@\n@U=UU\nUUUUU\nUUUUU", parking.toString());
-//        assertEquals(21, parking.getAvailableBaysCount());
-//
-//        assertEquals(11, parking.parkCar('M'));
-//        assertEquals("UUUUU\nC=CU@\n@M=UU\nUUUUU\nUUUUU", parking.toString());
-//        assertEquals(20, parking.getAvailableBaysCount());
-//
-//        assertEquals(13, parking.parkCar('M'));
-//        assertEquals("UUUUU\nC=CU@\n@M=MU\nUUUUU\nUUUUU", parking.toString());
-//        assertEquals(19, parking.getAvailableBaysCount());
-//
-//        assertEquals(10, parking.parkCar('D'));
-//        assertEquals("UUUUU\nC=CU@\nDM=MU\nUUUUU\nUUUUU", parking.toString());
-//        assertEquals(18, parking.getAvailableBaysCount());
-//
-//        assertEquals(5, parking.parkCar('D'));
-//        assertEquals("UUUUU\nC=CUD\nDM=MU\nUUUUU\nUUUUU", parking.toString());
-//        assertEquals(17, parking.getAvailableBaysCount());
-//
-//        assertEquals(-1, parking.parkCar('D'));
-//        assertEquals("UUUUU\nC=CUD\nDM=MU\nUUUUU\nUUUUU", parking.toString());
-//        assertEquals(17, parking.getAvailableBaysCount());
-//
-//        assertFalse(parking.unparkCar(3));
-//        assertEquals("UUUUU\nC=CUD\nDM=MU\nUUUUU\nUUUUU", parking.toString());
-//        assertEquals(17, parking.getAvailableBaysCount());
-//
-//        assertTrue(parking.unparkCar(13));
-//        assertEquals("UUUUU\nC=CUD\nDM=UU\nUUUUU\nUUUUU", parking.toString());
-//        assertEquals(18, parking.getAvailableBaysCount());
-//    }
+    @Test
+    public void testCompleteSolution() {
+        assertEquals(7, parking.parkCar('C'));
+        assertEquals("UUUUU\nU=CU@\n@U=UU\nUUUUU\nUUUUU", parking.toString());
+        assertEquals(22, parking.getAvailableBaysCount());
+
+        assertEquals(9, parking.parkCar('C'));
+        assertEquals("UUUUU\nC=CU@\n@U=UU\nUUUUU\nUUUUU", parking.toString());
+        assertEquals(21, parking.getAvailableBaysCount());
+
+        assertEquals(11, parking.parkCar('M'));
+        assertEquals("UUUUU\nC=CU@\n@M=UU\nUUUUU\nUUUUU", parking.toString());
+        assertEquals(20, parking.getAvailableBaysCount());
+
+        assertEquals(13, parking.parkCar('M'));
+        assertEquals("UUUUU\nC=CU@\n@M=MU\nUUUUU\nUUUUU", parking.toString());
+        assertEquals(19, parking.getAvailableBaysCount());
+
+        assertEquals(10, parking.parkCar('D'));
+        assertEquals("UUUUU\nC=CU@\nDM=MU\nUUUUU\nUUUUU", parking.toString());
+        assertEquals(18, parking.getAvailableBaysCount());
+
+        assertEquals(5, parking.parkCar('D'));
+        assertEquals("UUUUU\nC=CUD\nDM=MU\nUUUUU\nUUUUU", parking.toString());
+        assertEquals(17, parking.getAvailableBaysCount());
+
+        assertEquals(6, parking.parkCar('D'));
+        assertEquals("UUUUU\nC=CDD\nDM=MU\nUUUUU\nUUUUU", parking.toString());
+        assertEquals(16, parking.getAvailableBaysCount());
+
+        assertFalse(parking.unparkCar(3));
+        assertEquals("UUUUU\nC=CDD\nDM=MU\nUUUUU\nUUUUU", parking.toString());
+        assertEquals(16, parking.getAvailableBaysCount());
+
+        assertTrue(parking.unparkCar(13));
+        assertEquals("UUUUU\nC=CDD\nDM=UU\nUUUUU\nUUUUU", parking.toString());
+        assertEquals(17, parking.getAvailableBaysCount());
+
+        assertTrue(parking.unparkCar(10));
+        assertEquals("UUUUU\nC=CDD\n@M=UU\nUUUUU\nUUUUU", parking.toString());
+        assertEquals(18, parking.getAvailableBaysCount());
+    }
 }
